f501918377111bfa69ab52a816a9880b2617cb58
Ed Swierk <eswierk@bigswitch.com>
2011-04-29 15:42:57 -0700

    Run rpmbuild in a subdirectory so it doesn't blow away the original git source trees

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 sourcedir=$(dirname $0)
-builddir=$(pwd)
+builddir=$(pwd)/build
 
 set -ex
+
+rm -rf ${builddir}
+mkdir ${builddir}
 
 tar -C ${sourcedir}/../../../../..  -c -z -f ${builddir}/openflow.tar.gz --exclude=.git --exclude=.gitignore openflow
 tar -C ${sourcedir}/../../../../..  -c -z -f ${builddir}/snac.tar.gz --exclude=.git --exclude=.gitignore snac-nox
@@ -15,8 +18,8 @@ cat >${builddir}/.rpmmacros <<EOF
 %_builddir ${builddir}
 %_sourcedir ${builddir}
 %_specdir ${builddir}
-%_srcrpmdir ${builddir}
-%_rpmdir ${builddir}
+%_srcrpmdir ${builddir}/..
+%_rpmdir ${builddir}/..
 EOF
 
 HOME=${builddir} rpmbuild -ba "$@" ${builddir}/openflow.spec
